bde8aae7bbfd8f91a4a8e46d41c0a277943913f0
Usman Yousaf <mani.yousaf98@gmailcom>
2023-10-03 23:11:15 +0500

    Initial Commit

---
+++ resources/views/Emails/TestEmail.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>How to send mail using queue in Laravel </title>
+
+</head>
+
+<body>
+
+
+
+<p>Hi, Sir</p>
+
+<p>This is Test Mail.In this you will learn how to Send Email through Queue Job.</p>
+
+<strong>Usmanme : https://github.com/Usmanme</strong>
+</body>
+
+</html>
